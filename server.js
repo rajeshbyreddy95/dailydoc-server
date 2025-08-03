d0259f22431aed543035bdf476e1c3f240184c50
Byreddy Rajesh <rajeshbyreddy@Byreddys-MacBook-Air.local>
2025-08-03 14:26:07 +0530

    updated code

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-const express = require("express")
-const app = express()
-const dotenv = require("dotenv")
-const cors = require("cors")
-const connectDB = require("./db/config")
-const mongoose = require("mongoose")
-const authRoute = require("./routes/auth")
-const scheduleRoute = require("./routes/schedule")
-const profileRoute = require("./routes/profile")
-const runTaskReminderScheduler = require('./scheduler/taskReminder');
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./db/config");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const scheduleRoute = require("./routes/schedule");
+const profileRoute = require("./routes/profile");
+const runTaskReminderScheduler = require("./scheduler/taskReminder");
 
-dotenv.config()
+dotenv.config();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+connectDB();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use("/auth", authRoute);
+app.use("/schedule", scheduleRoute);
+app.use("/", profileRoute);
 
-connectDB()
-
-app.use("/auth", authRoute)
-app.use("/schedule", scheduleRoute)
-app.use("/",profileRoute)
-
-
-app.get("/",(req, res)=>{
-    res.json({"message":"server running"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "server running" });
+});
 const PORT = process.env.PORT || 8070;
-app.listen(PORT, ()=>{
-    console.log("server running under port ", PORT);
-console.log(process.env.EMAIL_USER)
-
-    runTaskReminderScheduler();
-})
+app.listen(PORT, () => {
+  console.log("server running under port ", PORT);
+  console.log(process.env.EMAIL_USER);
+  runTaskReminderScheduler();
+});
